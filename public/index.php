038c43bb0ba566b78cc1adfef5e9455a9eba8be1
Stayno-Cheffr <stayno@cheffr.com>
2025-06-24 19:03:47 +0200

    following up the course to 2.3

--- public/index.php
+++ public/index.php
@@ -1,6 +1,15 @@
 <?php
+
+    declare(strict_types=1);
     require_once '../Transaction.php';
 
-    $transaction = new Transaction();
+    $obj = new \stdClass();
+    $obj->name = 'Test Object';
+    $obj->age = 42;
 
-    var_dump($transaction);
+    var_dump($obj);
+
+    $transaction = new Transaction(100, 'Test Transaction');
+    $transaction->addTax(8)->applyDiscount(10);
+
+    var_dump($transaction->getAmount());
